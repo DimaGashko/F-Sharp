28ec664f2879f9470282103674b3049692fd5f61
DimaGashko <dmitry.gashko@postindustria.com>
2019-10-12 23:06:06 +0300

    first func

--- 0/0.fs
+++ 0/0.fs
@@ -3,3 +3,13 @@ let mutable a = 10
 a <- 5
 
 printf "%d" a
+
+let items = [1..10]
+
+let r = List.append items [6]
+
+let prefix prefixStr baseStr =
+    prefixStr + ", " + baseStr
+
+prefix "Hello" "Jeck"
+
