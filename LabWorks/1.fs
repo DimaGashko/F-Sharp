1f84b61321537fe49e9232f7fccda938066f02f7
DimaGashko <dmitry.gashko@postindustria.com>
2019-11-12 01:16:00 +0200

    1: tmp

--- LabWorks/1.fs
+++ LabWorks/1.fs
@@ -10,17 +10,17 @@ let rec fact x =
     else float x * fact (x - 1)
 
 let step x n = 
-    fact (n * 2) / pow 4. n / (fact >> pow2) n *
+    fact (n * 2) / (pow 4. n * (fact >> pow2) n) *
     (fun k -> (pow x k) / float k)(n * 2 + 1)
 
-let rec _asin x n eps =
+let rec _arcsin x n eps =
     let v = step x n
 
     if abs v < eps then v
-    else v + _asin x (n + 1) eps
+    else v + _arcsin x (n + 1) eps
 
-let asin x =
-    _asin x 0 1e-9
+let arcsin x =
+    _arcsin x 0 1e-9
 
 asin 0.0
 asin 0.5 // 0
