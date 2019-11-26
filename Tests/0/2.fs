bd6a95e4a862b5a413fb5ec53ffd0343bf614688
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-11-26 22:35:24 +0200

    tests

--- Tests/0/2.fs
+++ Tests/0/2.fs
@@ -1,6 +1,19 @@
+let rec len x = 
+    if List.isEmpty x then 0
+    else 1 + len (List.tail x)
 
-match [2;1;3] with 
-| [2;hhh;y] -> printfn "x=%A y=%A" hhh y
-| 1::tail -> printfn "tail=%A" tail 
-| [] -> printfn "empty"
-| _ -> printfn "else"
+let rec len2 = function
+    | [] -> 0
+    | _::tl -> 1 + len2 tl
+
+let isVowels = function
+    |'A'|'E'|'I'|'O'|'U'|'a'|'e'|'i'|'o'|'u' -> true
+    | _ -> false
+
+let rec countVowels = function
+    [] -> 0
+    | h::t when isVowels h -> 1 + countVowels t
+    | _::t -> countVowels t
+    
+
+countVowels (List.ofSeq "aeiouyyy555oooo")
