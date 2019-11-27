7fbacfd78981ab5b35d918f02489a247f4243dca
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-11-27 08:09:56 +0200

    l2: part one

--- LabWorks/2.fs
+++ LabWorks/2.fs
@@ -1,6 +1,13 @@
-let rec mulOfNegs = function
-    | [] -> 0
-    | x::t when x < 0 -> x * if mulOfNegs t <> 0 then mulOfNegs t else 1
-    | _::t -> mulOfNegs t
+let rec productOfNegs = function
+    | [] -> 1
+    | x::t when x < 0 -> x * productOfNegs t
+    | _::t -> productOfNegs t
 
-mulOfNegs [2;3;-1;-2;-3]
+productOfNegs [-1;-2;-3]
+productOfNegs [2;3;-2;-5]
+productOfNegs [1;2;3;-5;4]
+productOfNegs [1;2;3;4]
+productOfNegs []
+
+// https://math.stackexchange.com/questions/110546/what-is-the-product-of-the-empty-set
+// https://en.wikipedia.org/wiki/Empty_product
