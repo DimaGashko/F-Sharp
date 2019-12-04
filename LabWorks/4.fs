ae4c417053c7c223fe1f039115d5297718d1c862
Dmitry Gashko <dimagashko@gmail.com>
2019-12-04 16:49:21 +0300

    refactor

--- LabWorks/4.fs
+++ LabWorks/4.fs
@@ -1,7 +1,3 @@
-let rec genlist n clo =
-   if n = 0 then []
-   else clo()::genlist (n-1) clo
-
 let sinCosGen (initialAngle: float) step =
    let mutable angle = initialAngle - step;
    fun() -> (angle <- angle + step; (sin angle, cos angle))
@@ -9,6 +5,10 @@ let sinCosGen (initialAngle: float) step =
 let sinCosGenRef (initialAngle: float) step =
    let angle = ref (initialAngle - step);
    fun() -> (angle := !angle + step; (sin !angle, cos !angle))
+
+let rec genlist n clo =
+   if n = 0 then []
+   else clo()::genlist (n-1) clo
 
 genlist 10 (sinCosGen 0.0 (System.Math.PI / 4.))
    |> List.map (fun x -> sprintf "\t(%.3f, %.3f)\n" (fst x) (snd x))
